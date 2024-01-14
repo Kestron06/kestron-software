87eb7dab551746f62a57a7c3289b207c64e52543
Kestron06 <kestron@kestron.software>
2024-01-13 16:17:46 -0800

    Overhaul

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const fetch=require("node-fetch");
 const express=require("express");
 const fs=require("fs");
 const path=require("path");
 let site=new express();
 site.use(express.static(path.join(__dirname + '/static')));
-site.use('/favicon.ico', express.static('static/images/bario.png'))
-site.listen(3000,()=>{
+site.use('/favicon.ico', express.static('static/images/kestron.png'))
+site.listen(80,()=>{
     console.log("Site online");
 });
 site.get('/about', async (req, res) => {
-	res.send(fs.readFileSync('static/about.html', 'utf-8'));
-});
-site.get('/contact', async (req, res) => {
-	res.send(fs.readFileSync('static/contact.html', 'utf-8'));
+	res.send(fs.readFileSync('static/index.html', 'utf-8'));
 });
 site.get('/ethics', async (req, res) => {
 	res.send(fs.readFileSync('static/ethics.html', 'utf-8'));
@@ -22,9 +18,6 @@ site.get('/links', async (req, res) => {
 });
 site.get('/projects', async (req, res) => {
 	res.send(fs.readFileSync('static/projects.html', 'utf-8'));
-});
-site.get('/support', async (req, res) => {
-	res.send(fs.readFileSync('static/support.html', 'utf-8'));
 });
 site.get('/*',async (req,res)=>{
     res.send("<script>document.location.replace('https://kestron.software/');</script>");
--- static/headAndFoot.js
+++ static/headAndFoot.js
@@ -1,14 +1,13 @@
 const banner=`
 <div id="banner">
-    <img src='images/banner.png'><br>
-    <a href='/'>Home</a><a href='/about'>About</a><a href='/projects'>Projects</a><a href='/links'>Links</a><a href='/ethics'>Ethics</a><a href='/support'>Support Kestron</a>
+    <a href="/"><img src='images/kestron.png' id="kestronIcon"><img src='images/banner.png' id="kestronBanner"></a><br>
+    <a href='/about' class="bannerBtn">About</a><a href='/projects' class="bannerBtn">Projects</a><a href='/links' class="bannerBtn">Links</a><a href='/ethics' class="bannerBtn">Ethics</a>
 </div>
 <br><br>
 `;
 const footer=`
-<hr>
 <footer>
-    ©Kestron 2023 <a href='/contact'>Contact/Support</a>.
+    <a href='mailto:kestron@kestron.software'>Kestron@Kestron.software</a> <a href="https://discord.gg/k3yVkrrvez">Discord Support Server</a>
 </footer>
 `;
 document.body.innerHTML=banner+document.body.innerHTML+footer;

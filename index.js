60cb150d3edcd88d2472e79461db51c53afb0919
Kestron <kestron@kestron.software>
2025-02-27 23:36:56 -0800

    EJS and Kestron.com move

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const ejs=require("ejs");
 const express=require("express");
 const fs=require("fs");
 const path=require("path");
@@ -7,21 +8,33 @@ site.use('/favicon.ico', express.static('static/images/kestron2.png'))
 site.listen(3000,()=>{
     console.log("Site online");
 });
-site.get('/about', async (req, res) => {
-	res.send(fs.readFileSync('static/index.html', 'utf-8'));
+var ejsGlobals={
+	"header":`
+<div id="banner">
+    <a href="/"><img src='images/kestron2.png' id="kestronIcon"><img src='images/banner.png' id="kestronBanner"></a><br>
+    <a href='/about' class="bannerBtn">About</a><a href='/projects' class="bannerBtn">Projects</a><a href='/links' class="bannerBtn">Links</a><a href='/ethics' class="bannerBtn">Ethics</a>
+</div>
+<br><br>`,
+	"footer":`
+<footer>
+    <a href='mailto:kestron@kestron.software'>kestron@kestron.software</a> | <a href="https://discord.gg/k3yVkrrvez">Discord Support Server</a>
+</footer>
+`
+};
+fs.readdirSync("./ejs").forEach(file=>{
+	site.get(`/${file.split(".")[0]}`,async (req,res)=>{
+		ejsGlobals.curDomain=`${req.protocol}://${req.get('host')}${req.originalUrl}`;
+		ejs.renderFile(`./ejs/${file}`, ejsGlobals, {}, function(err, str) {
+			res.send(str);
+		});
+	});
 });
-site.get('/ethics', async (req, res) => {
-	res.send(fs.readFileSync('static/ethics.html', 'utf-8'));
-});
-site.get('/links', async (req, res) => {
-	res.send(fs.readFileSync('static/links.html', 'utf-8'));
-});
-site.get('/projects', async (req, res) => {
-	res.send(fs.readFileSync('static/projects.html', 'utf-8'));
-});
-site.get('/admin', async (req, res) => {
-	res.send(fs.readFileSync('static/admin.html', 'utf-8'));
+site.get(`/`,async (req,res)=>{
+	ejsGlobals.curDomain=`${req.protocol}://${req.get('host')}${req.originalUrl}`;
+	ejs.renderFile(`./ejs/about.ejs`, ejsGlobals, {}, function(err, str) {
+		res.send(str);
+	});
 });
 site.get('/*',async (req,res)=>{
-    res.send("<script>document.location.replace('https://kestron.software/');</script>");
+    res.send("<script>document.location.replace('https://kestron.com/');</script>");
 });
